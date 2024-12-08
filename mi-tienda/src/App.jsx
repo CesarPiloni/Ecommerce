851ef35f3f0e707145c019359a7795a0ecdf24d0
CesarPiloni <Cesar_9603@hotmail.com>
2024-12-08 03:48:57 -0300

    NavegaLasRutasPiloni

--- mi-tienda/src/App.jsx
+++ mi-tienda/src/App.jsx
@@ -1,14 +1,25 @@
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/layouts/navBar/NavBar";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import ItemDetail from "./components/pages/itemDetail/ItemDetail";
+import Cart from "./components/pages/cart/Cart";
+import Checkout from "./components/pages/checkout/Checkout";
+import Footer from "./components/layouts/footer/Footer";
 
 function App() {
   return (
-    <>   
-      <NavBar/>
-      <ItemListContainer/>
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:name" element={<ItemListContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/itemDetail/:id" element={<ItemDetail />} />
+        <Route path="/checkout" element={<Checkout/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ mi-tienda/src/components/layouts/navBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import CartWidget from "../../common/cartWidget/CartWidget";
+
+const Navbar = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "12px",
+        backgroundColor: "steelblue",
+      }}
+    >
+      <Link to="/">logo</Link>
+      <ul
+        style={{
+          display: "flex",
+          listStyle: "none",
+          gap: "20px",
+        }}
+      >
+        <Link to="/category/teclados">Teclados</Link>
+        <Link to="/category/mouse">Mouses</Link>
+      </ul>
+      <CartWidget />
+    </div>
+  );
+};
+
+export default Navbar;
